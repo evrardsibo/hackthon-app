b9daa0cb1a99cd6dd2492c40afb1e665488cb938
MathisVkg <mvk181@gmail.com>
2021-10-23 12:51:44 +0200

    fix

--- src/components/Restau.jsx
+++ src/components/Restau.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useEffect } from 'react'
 
 export default function Restau() {
-    const URL = 'https://www.odwb.be/explore/dataset/horeca/api/?lang=fr&start=0#:~:text=/api/records/1.0/search/%3Fdataset%3Dhoreca%26q%3D%26lang%3Dfr%26start%3D0%26facet%3Dnomenclature_de_pois'
+    const URL = 'https://www.odwb.be/api/records/1.0/search/?dataset=horeca&q=&lang=fr&rows=34&facet=nomenclature_de_pois'
 
     async function fetch() {
         return await axios.get(URL)
