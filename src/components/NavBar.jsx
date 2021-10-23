840c4dbd97f1e7b6feec4a7d7fdc6997b8450e46
MathisVkg <mvk181@gmail.com>
2021-10-23 08:32:51 +0200

    nab bar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+import NavLink from 'react-router-dom';
+import { AiOutlineHome } from 'react-icons/ai';
+import { RiCommunityLine } from 'react-icons/ri';
+import { BsBinoculars } from 'react-icons/bs';
+import { BsLightbulb } from 'react-icons/bs';
+
+const NavBar = () => {
+    let icons = { className: 'icons' }
+
+    return (
+        <header>
+            <nav className="menu">
+                <NavLink to='./'>
+                    <IconContext.Provider value={ icons }><AiOutlineHome /></IconContext.Provider>
+                </NavLink>
+                <NavLink to='./'>
+                    <IconContext.Provider value={ icons }><RiCommunityLine /></IconContext.Provider>
+                </NavLink>
+                <NavLink to='./'>
+                    <IconContext.Provider value={ icons }><BsBinoculars /></IconContext.Provider>
+                </NavLink>
+                <NavLink to='./'>
+                    <IconContext.Provider value={ icons }><BsLightbulb /></IconContext.Provider>
+                </NavLink>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
