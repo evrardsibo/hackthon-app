6c1fd3069dbb4de4d8962f0d7603e721ec613c46
svanna <anais.dehand@gmail.com>
2021-10-23 22:45:54 +0200

    home update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../assets/scss/Home.css'
 
 const Home = () => {
     return (
         <div>
+            
             <Link className="commerces" to="/commerces">Commerces</Link>
             <Link className="commerces" to="/restaurants">Restaurants</Link>
         </div>
