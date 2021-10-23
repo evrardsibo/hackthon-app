5b92d419e3a043d18972744fee6f46997f809236
MathisVkg <mvk181@gmail.com>
2021-10-23 23:12:05 +0200

    carousel

--- src/components/Commerces.jsx
+++ src/components/Commerces.jsx
@@ -1,16 +1,53 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import Carousel from 'react-elastic-carousel'
+import { NavLink } from 'react-router-dom'
 
 const Commerces = () => {
+    const [itemToShowCarou, setItemToShowCarou] = useState(1)
     return (
-        <div>
-            commerces
-        </div>
+        <main>
+            <MoreInfo />
+            <GetCarousel />
+        </main>
     )
 
-    function Carousel() {
+    function MoreInfo() {
         return (
-            <div className=""></div>
+            <div className="infoContainer">
+                Choisissez un type de commerces
+            </div>
+        );
+    }
+
+    function GetCarousel() {
+        return (
+            <div className="container">
+                <Carousel
+                    itemsToShow={itemToShowCarou}
+                    showArrows={false}
+                    pagination={false}
+                    className="movieList"
+                >
+                    <div className="card">
+                        <NavLink to="/" className="title">Alimentation</NavLink>
+                    </div>
+                    <div className="card">
+                        <NavLink to="/" className="title">textile</NavLink>
+                    </div>
+                    <div className="card">
+                        <NavLink to="/" className="title">Pharmacie</NavLink>
+                    </div>
+                    <div className="card">
+                        <NavLink to="/" className="title">Boulangerie</NavLink>
+                    </div>
+                    <div className="card">
+                        <NavLink to="/" className="title">Soins du corps</NavLink>
+                    </div>
+                    <div className="card">
+                        <NavLink to="/" className="title">Librairie</NavLink>
+                    </div>
+                </Carousel>
+            </div>
         );
     }
 }
