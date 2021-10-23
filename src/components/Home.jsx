cfabe93fa70a818b23d5e2ac796f7f3d7047c03f
svanna <anais.dehand@gmail.com>
2021-10-23 23:12:30 +0200

    scss

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import '../assets/scss/Home.css'
 const Home = () => {
     return (
         <div>
-            
+            <p className="intro">Découvrez nos services locaux:</p>
             <Link className="commerces" to="/commerces">Commerces</Link>
             <Link className="commerces" to="/restaurants">Restaurants</Link>
         </div>
