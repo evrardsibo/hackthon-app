eca3927ad9a9f8b21bbaa71a608a34166a7d7a2d
MathisVkg <mvk181@gmail.com>
2021-10-24 00:42:23 +0200

    details page com

--- src/Pages/commerces/snackDetails.jsx
+++ src/Pages/commerces/snackDetails.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
-import {AiOutlineArrowLeft} from "react-icons/all";
+import {AiOutlineArrowLeft} from "react-icons/all"
+import { BiUpArrow } from "react-icons/bi"
 
-const SnackDetails = () => {
+const SnackDetails = (props) => {
+    const stockData = [...props.rest]
     return (
         <div>
             <AiOutlineArrowLeft onClick={() => window.history.back()} />
-            snack
+            <GetCardData />
         </div>
     )
+
+    function GetCardData() {
+        return (
+            <div className="container">
+                {
+                    stockData.filter(snack => snack.fields.nomenclature_de_pois == "Snacks / Friteries").map((snack) => {
+                        return (
+                            <div className="card" key={ snack.recordid }>
+                                <h2 className="title">{ snack.fields.titre }</h2>
+                                <span className="iconArrow"><BiUpArrow /></span>
+                                <div className="info">
+                                    <p className="adresse">{ snack.fields.adresse }</p>
+                                    <p className="number">{ snack.fields.numero_de_telephone }</p>
+                                </div>
+                            </div>
+                            )
+                    })
+                }
+            </div>
+        );
+    }
 }
 
 export default SnackDetails
